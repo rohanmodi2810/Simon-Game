37f9cfe7a564d583382cdf9448aa4873fbb91b06
rohan modi <modirohan28@gmail.com>
2020-06-14 09:28:07 +0530

    version-1

---
+++ game.js
@@ -0,0 +1,80 @@
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
+
+var gamePattern = [];
+
+var userclickedPattern = [];
+
+var started = false;
+
+var level=0;
+
+$(document).keydown(function(){
+    if (!started) {
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+})
+
+$('.btn').click(function(){
+    var userChosenColor = $(this).attr('id');
+    userclickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userclickedPattern.length-1);
+});
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userclickedPattern[currentLevel]) {
+        if (gamePattern.length === userclickedPattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound('wrong');
+
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
+function nextSequence() {
+    userclickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level "+level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColor);
+}
+
+function playSound(name) {
+    var audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
